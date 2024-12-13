097e908f5d61c963ae69c1d10f24f6302c304c2c
Kier Roman <kierroman@gmail.com>
2024-12-12 22:01:37 -0500

    Game Functioning...need timer possibly and better CSS

--- JS/mole.js
+++ JS/mole.js
@@ -1,39 +1,82 @@
-const board = document.querySelector('#board')
-const scoreEl = document.querySelector('#score')
-const help = document.querySelector('#help')
+const board = document.querySelector('#board');
+const scoreEl = document.querySelector('#score');
+const topEl = document.querySelector('h1')
+const help = document.querySelector('#help');
+let startEl = document.querySelector('#start')
+let score = 0;
+// let time = 60;
 let thisTitan;
+let thisScout;
 
-window.onload = function(){
-    popUp();
-}
+startEl.addEventListener('click', () => {
+    startEl.style.display = 'none';
+    game();
+})
+// window.onload = function () {
+//     game();
+// console.log(startEl)
+// }
 
-const popUp = () => {
-    for (let i = 0; i < 9; i++){
+let game = () => {
+    for (let i = 0; i < 9; i++) {
         let hole = document.createElement('div');
         hole.id = i.toString();
-        document.getElementById('board').appendChild(hole)
+        document.querySelector('#board').appendChild(hole);
     }
-    setInterval(, 1000)
+    // setInterval(timer, )
+    setInterval(popTitan, 1500);
+    setInterval(popScout, 2200);
 }
 
-const randomHole = () => {
-    let num = Math.floor(Math.random()* 9);
+let randomHole = () => {
+    let num = Math.floor(Math.random() * 9);
     return num.toString();
 }
 
 
-
-const popTitan = () => {
+let popTitan = () => {
+    if (topEl.innerText == 'GAME OVER!') {
+        return;
+    }
+    if (thisTitan) {
+        thisTitan.innerHTML = '';
+    }
     let titan = document.createElement('img');
     titan.src = 'Uggo-Titan.png';
 
     let num = randomHole();
+
+    if (thisScout && thisScout.id == num) {
+        return;
+    }
     thisTitan = document.getElementById(num);
-    thisTitan.appendChild(titan)
+    titan.addEventListener('click', () => {
+        score += 10
+        scoreEl.innerHTML = score
+    })
+    thisTitan.appendChild(titan);
 }
 
-//Try to start game with page load...
+let popScout = () => {
+    if (topEl.innerText == 'GAME OVER!') {
+        return;
+    }
+    if (thisScout) {
+        thisScout.innerHTML = '';
+    }
+    let scout = document.createElement('img');
+    scout.src = 'Mikasa.png';
 
-// function time() {
-//     c
-// }
+    let num = randomHole();
+
+    if (thisTitan && thisTitan.id == num) {
+        return;
+    }
+    thisScout = document.getElementById(num);
+    scout.addEventListener('click', () => {
+        topEl.innerHTML = 'GAME OVER!'
+    })
+    thisScout.appendChild(scout);
+
+}
+let helpMe = () => { }
